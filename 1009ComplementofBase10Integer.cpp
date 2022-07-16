bae8dea8d2e7ef8a4f83025ea3a1b95c5de795a2
meshramtushar52 <109428259+meshramtushar52@users.noreply.github.com>
2022-07-16 22:11:34 +0530

    Add files via upload
    1009. Complement of Base 10 Integer

---
+++ 1009ComplementofBase10Integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        int a = n;
+        int mask = 0;
+        
+        if(n == 0){
+            return 1;
+        }
+        while (a != 0){
+            mask = (mask << 1) | 1;
+            a = a >> 1;
+        }
+        int ans = (~n) & mask;
+        return ans;
+    }
+};
